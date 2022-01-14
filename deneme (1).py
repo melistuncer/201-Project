37ecc518c3467197a1601bd6a281d4715a7164d5
Melis Tuncer <92749876+melistuncer@users.noreply.github.com>
2022-01-14 13:28:38 +0300

    Update deneme (1).py

--- deneme (1).py
+++ deneme (1).py
@@ -1,319 +1,189 @@
-from os import replace, spawnl
-import random
-import sys, pygame
-from pygame.event import get
-from pygame.time import Clock, get_ticks
-from pygame.transform import scale
-import time
-from pygame import mixer
+from ast import Str
+import sys, pygame, time, random
 pygame.init()
-random.seed()
-clock = pygame.time.Clock
+pygame.font.init()
 
-black = 0, 0, 0
-white = 255, 255, 255
-size = width, height = 1000, 780
-screen = pygame.display.set_mode(size)
-mixer.music.load("backgroundMusic.mp3")
-mixer.music.play(-1)
-speed = 1
-
-posx_list = [75,275,475,675]
-lane_posx_list = [75,275,475,675]
-list = []
-
-def random_picker():
-    while len(list) < 4:
-        item = random.choice(posx_list)
-        list.append(item)
-        posx_list.remove(item)
-    return list
-
-random_picker()
 
 class Game_Launcher:
+    CAR_IMAGE = pygame.image.load("greencar.png")
+    COIN_IMAGE = pygame.image.load("coin.png")
+    HOLE_IMAGE = pygame.image.load("hole.png")
+    size = width, height = 1000, 780
+    screen = pygame.display.set_mode(size)
+    background = pygame.transform.scale(pygame.image.load("asphalt.jpg"), size)
+
+    posx_list = [75,275,475,675]
+    lane_posx_list = [75,275,475,675]
+    list = [1, 2, 3, 4]
+
+    CAR = []
+    STRIPS = []
+    OBJECTS = []
+    
 
     def __init__(self, value):
         self._bool = value
-    
-    def game_working(self):
-        screen.fill(black)
-        for i in my_strips:
-            i.move()
-            i.show()
-        for i in game_objects:
-            i.move()
-            i.show()
-        tree.lose_health()
-        hole.lose_health()
-        coin.get_score()
-        health.show()
-        score.show()
-        gas.lose_fuel()
-        fuel.full()
-        gas.show()
-        if coin._posy < 780 and coin._posy > 774:
-            posx_list.append(75)
-            posx_list.append(275)
-            posx_list.append(475)
-            posx_list.append(675)
-            list.clear()
-            random_picker()
-            coin._posx = list[0]
-            fuel._posx = list[1]
-            tree._posx = list[2]
-            hole._posx = list[3]
-            coin.selectlane()
-            fuel.selectlane()
-            tree.selectlane()
-            hole.selectlane()
-            
-class Grass():
-    def __init__(self):
-        self._image = pygame.image.load("grass.jpeg")
-        self._sized = pygame.transform.smoothscale(self._image, (200,1560))
-        self._screen = screen
-        self._posx = 800
-        self._posy = -780
-        self._speedy= speed
-    
-    def show(self):
-        screen.blit(self._sized, (self._posx, self._posy))
-    
-    def move(self):
-        self._posy += self._speedy
-        if self._posy == 0:
-            self._posy = -780
 
-class Strip():
-    def __init__(self, posx, posy):
-        self._screen = screen
-        self._posx = posx
-        self._posy = posy
-        self._speedy = speed
-        self.show()
-        self.move()
-           
-    def show(self):
-        pygame.draw.rect(self._screen, (255,255,255), (self._posx, self._posy,10,50))
-    
-    def move(self):
-        self._posy += self._speedy
-        if self._posy == 780:
-            self._posy = 0
+    def create_objects(self):
+        car = Car(450, 450)
+        Game_Launcher.CAR.append(car)
+        strip1 = Strip(195, 0, 1, Game_Launcher.screen)
+        strip2 = Strip(395, 0, 1, Game_Launcher.screen)
+        strip3 = Strip(595, 0, 1, Game_Launcher.screen)
+        strip_4 = Strip(195,260, 1, Game_Launcher.screen)
+        strip_5 = Strip(395,260, 1, Game_Launcher.screen)
+        strip_6 = Strip(595,260, 1, Game_Launcher.screen)
+        strip_7 = Strip(195,520, 1, Game_Launcher.screen)
+        strip_8 = Strip(395,520, 1, Game_Launcher.screen)
+        strip_9 = Strip(595,520, 1, Game_Launcher.screen)
+        Game_Launcher.STRIPS.extend([strip1, strip2, strip3, strip_4, strip_5, strip_6, strip_7, strip_8, strip_9])
+        coin = Coin(0, Game_Launcher.COIN_IMAGE, Game_Launcher.list, 1, Game_Launcher.screen)
+        Game_Launcher.OBJECTS.append(coin)
 
-class Car():
-    def __init__(self, posx, posy):
-        self._image = pygame.image.load("car.png")
-        self._sized = pygame.transform.smoothscale(self._image, (100,200))
-        self._screen = screen
-        self._posy = posy
-        self._posx = posx
-        self.show()
-        self._state = 'straight'
-        self.move()
+
+    def working(self):
+        self._bool = True
+
+    def not_working(self):
+        self._bool = False
+        
+    def move_car_left(self):
+        for car in Game_Launcher.CAR:
+            car.move_left()
+
+    def move_car_right(self):
+        for car in Game_Launcher.CAR:
+            car.move_right()
+
+    def blit_window(self):
+
+        FPS = 60  # shows 60 frames per second
+        level = 1
+        health = 10
+        fuel = 20
+        main_font = pygame.font.SysFont("timesnewroman", 30)
+
+        clock = pygame.time.Clock()
+        Game_Launcher.screen.blit(Game_Launcher.background, (0,0))
+        health_label = main_font.render(f"Health: {health}", 1, (255,255,255))
+        fuel_label = main_font.render(f"Fuel: {fuel}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        Game_Launcher.screen.blit(health_label,(10,10))
+        Game_Launcher.screen.blit(fuel_label,(680,10))
+        Game_Launcher.screen.blit(level_label,(350,10))
+
+        for car in Game_Launcher.CAR:
+            car.show(Game_Launcher.screen, pygame.transform.smoothscale(Game_Launcher.CAR_IMAGE, (100,200)))
+        for item in Game_Launcher.STRIPS:
+            item.show()
+            item.move()
+        for object in Game_Launcher.OBJECTS:
+            object.move()
+            object.show()
+        pygame.display.update()
+
+
+class Car:
+    def __init__(self, x, y, health =100):
+        self._posx = x
+        self._posy = y
+        self._health = health
+        self._image = None
+        self._sized_image = None
         self._lane = 3
 
-    def show(self):
-        screen.blit(self._sized, (self._posx, self._posy))
+    def show(self, screen,  image):
+        screen.blit(image, (self._posx, self._posy))
 
-    def move(self):
-        if self._state == 'right':
-            self._posx += 200
-        elif self._state == 'left':
+    def give_posx(self):
+        return self._posx
+
+    def move_left(self):
+        if self._posx -200 > 0:
             self._posx -= 200
+            self._lane -= 1
+        else:
+            pass
+        return self._posx
     
-class Health():
-    def __init__(self):
-        self._image = pygame.image.load("heart1.png")
-        self._sized = pygame.transform.smoothscale(self._image, (50,50))
-        self._amount = 3
-    
-    def show(self):
-        if self._amount == 3:
-            screen.blit(self._sized, (800, 100))
-            screen.blit(self._sized, (860, 100))
-            screen.blit(self._sized, (920, 100))
-        elif self._amount == 2:
-            screen.blit(self._sized, (800, 100))
-            screen.blit(self._sized, (860, 100))
-        elif self._amount == 1:
-            screen.blit(self._sized, (800, 100))
+    def move_right(self):
+        if self._posx + 200 < 800:
+            self._posx += 200
+            self._lane += 1
+        else:
+            pass
 
-class Score():
-    def __init__(self):
-        self._posx = 100
-        self._posy = 100
-        self._score = 0
-
-    def show(self):
-        font = pygame.font.Font('freesansbold.ttf', 32)
-        score = str(self._score)
-        text = font.render(score, True, white)
-        screen.blit(text, (900, 50))
-
-class Gas():
-    def __init__(self):
-        self._posx = 100
-        self._posy = 100
-        self._amount = 100
-
-    def show(self):
-        font = pygame.font.Font('freesansbold.ttf', 22)
-        fuel_left = str("%.2f" % self._amount)
-        text1 = font.render("Remaining fuel: ", True, white)
-        text = font.render(fuel_left, True, white)
-        screen.blit(text1, (810, 700))
-        screen.blit(text, (870, 740))
-
-    def lose_fuel(self):
-        self._amount -= (100) * (start - finish)
-
-class Objects():
-    def __init__(self):
-        self._screen = screen
-        self._posy = 0
+class Strip:
+    def __init__(self, x, y, speed, screen):
+        self._posx = x
+        self._posy = y
         self._speedy = speed
-        self._possible_posx = [75,275,475,675]
-        self._list = list
+        self._screen = screen
+
+    def show(self):
+        pygame.draw.rect(self._screen, (255,255,255), (self._posx, self._posy,10,50))
 
     def move(self):
         self._posy += self._speedy
         if self._posy == 780:
             self._posy = 0
-    
-    def selectlane(self):
-        self._lane = self._possible_posx.index(self._posx) + 1
+
+class Objects:
+    def __init__(self, y, image, list, speed, screen):
+        self._posy = y
+        self._image = image
+        self._scaled = pygame.transform.smoothscale(self._image, (50,50))
+        self._screen = screen
+        self._possible_posx = [75,275,475,675]
+        self._lane_list = list
+        self._speedy = speed
+
+    def move(self):
+        self._posy += self._speedy
+        if self._posy == 780:
+            self._posy = 0
+
+    #def select_lane(self):
+     #   self._lane = self._possible_posx.index(self._posx) + 1
+
+    #def change_lane(self, list):
+    #    self._posx = list[0]
+
+    def show(self):
+        self._screen.blit(self._scaled, (self._posx, self._posy))
+
+    def hide_self(self):   # objects will disappear when car hits them
+        pass
 
 class Coin(Objects):
-    def __init__(self):
-        super().__init__()
-        self._image = pygame.image.load("coin.png")
-        self._scaled = pygame.transform.smoothscale(self._image, (50,50))
-        self._posx = self._list[0]
-        self.selectlane()
-    
-    def show(self):
-        screen.blit(self._scaled, (self._posx, self._posy))
+    def __init__(self, y, image, list, speed, screen):
+        super().__init__(y, image, list, speed, screen)
+        self._posx = self._lane_list[0]
+        #self.select_lane()
 
-    def get_score(self):
-        if self._posy == car._posy:
-            if car._lane == self._lane:
-                score._score += 1
+    def change_lane(self, list):
+        self._posx = list[0]
 
-class Fuel(Objects):
-    def __init__(self):
-        super().__init__()
-        self._image = pygame.image.load("fuel.png")
-        self._scaled = pygame.transform.smoothscale(self._image, (50,50))
-        self._posx = self._list[1]
-        self.selectlane()
-
-    def show(self):
-        screen.blit(self._scaled, (self._posx, self._posy))
-
-    def full(self):
-        if self._posy == car._posy:
-            if car._lane == self._lane:
-                gas._amount = 100
-
-class Obstacles(Objects):
-    def __init__(self):
-        super().__init__()
-    
-    def lose_health(self):
-        if self._posy == car._posy:
-            if car._lane == self._lane:
-                health._amount -= 1
-        
-class Tree(Obstacles):
-    def __init__(self):
-        super().__init__()
-        self._image = pygame.image.load("tree.png")
-        self._scaled = pygame.transform.smoothscale(self._image, (50,50))
-        self._posx = self._list[2]
-        self.selectlane()
-
-    def show(self):
-        screen.blit(self._scaled, (self._posx, self._posy))
-
-class Hole(Obstacles):
-    def __init__(self):
-        super().__init__()
-        self._image = pygame.image.load("hole.png")
-        self._scaled = pygame.transform.smoothscale(self._image, (50,50))
-        self._posx = self._list[3]
-        self.selectlane()
-
-    def show(self):
-        screen.blit(self._scaled, (self._posx, self._posy))
-    
-#road cars
-strip_1 = Strip(195,0)
-strip_2 = Strip(395,0)
-strip_3 = Strip(595,0)
-strip_4 = Strip(195,260)
-strip_5 = Strip(395,260)
-strip_6 = Strip(595,260)
-strip_7 = Strip(195,520)
-strip_8 = Strip(395,520)
-strip_9 = Strip(595,520)
-my_strips = [strip_1, strip_2, strip_3, strip_4, strip_5, strip_6, strip_7, strip_8, strip_9]
-#car
-car = Car(450, 450)
-health = Health()
-score = Score()
-gas = Gas()
-#interrection
-coin = Coin()
-fuel = Fuel()
-hole = Hole()
-tree = Tree()
-#decorations
-grass = Grass()
-game_objects = [grass, car, fuel, coin, tree, hole]
 playing = Game_Launcher(False)
 
-def lose_game():
-    pygame.draw.rect(screen, white, (500,500,300,300))
-    font = pygame.font.Font('freesansbold.ttf', 32)
-    text = font.render("u suck", True, white)
-    screen.blit(text, (150, 150))
-
-
 while 1:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
+                
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
-                playing._bool = True
+                playing.create_objects()
+                playing.working()
 
             if event.key == pygame.K_a:
-                if car._posx -200 > 0:
-                    car._posx -= 200
-                    car._lane -= 1
-                else:
-                    pass
+                playing.move_car_left()
 
             if event.key == pygame.K_d:
-                if car._posx + 200 < 800:
-                    car._posx += 200
-                    car._lane += 1
-                else:
-                    pass
+                playing.move_car_right()
 
-    if health._amount == 0:
-        lose_game()
-        playing._bool = False
-    
-    if gas._amount <= 0:
-        lose_game()
-        playing._bool = False
-
-    start = time.time()
     if playing._bool:  
-        playing.game_working()
-    finish = time.time()
+        playing.blit_window()
 
     pygame.display.update()
